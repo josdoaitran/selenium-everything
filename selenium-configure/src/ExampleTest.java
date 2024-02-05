75b2d5b77be94ad2f0e9d04c3cb4c7e38d50e8ff
Doai Tran <josdoaitran@gmail.com>
2024-02-05 10:56:58 +0700

    example java selenium with chromeoptions

--- selenium-configure/src/ExampleTest.java
+++ selenium-configure/src/ExampleTest.java
@@ -4,6 +4,11 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class ExampleTest {
     WebDriver driver;
@@ -14,13 +19,32 @@ public class ExampleTest {
 
     @BeforeEach
     void setup() {
-        driver = new ChromeDriver();
+        // Create ChromeOptions instance
+        ChromeOptions options = new ChromeOptions();
+        // Set desired Chrome options
+        options.addArguments("--disable-infobars"); // Disable the 'Chrome is being controlled by automated test software' notification
+        options.addArguments("--incognito"); // Open the browser in incognito mode
+
+        // Specify the download folder path
+        String downloadFolderPath = "/path/to/download/folder";
+        // Set the download directory
+        options.addArguments("download.default_directory=" + downloadFolderPath);
+        // 2 ways to block 3rd party cookies
+        // Option 1
+        options.addArguments("--block-third-party-cookies");
+        // Option 2
+
+//        Map<String, Object> prefs = new HashMap<>();
+//        prefs.put("profile.block_third_party_cookies", true);
+//        options.setExperimentalOption("prefs", prefs);
+
+        driver = new ChromeDriver(options);
         driver.get("http://automationtesting.in/");
     }
 
     @AfterEach
     void teardown() {
-        driver.quit();
+//        driver.quit();
     }
 
     @Test
